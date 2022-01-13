f54f5fa3447b68003c6c2ecd280de79ac9c8bda7
Bruno Moreira <bruno_moreira.19@outlook.com>
2022-01-12 23:21:18 -0300

    configurando eslint e definindo padroes para o codigo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-   response.send("Relou uordi!")
-})
+  response.send('Hello Word!!!');
+});
 
 app.listen(3000, () => console.log('Subiu!'));
